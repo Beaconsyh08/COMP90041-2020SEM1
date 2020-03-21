061f13cea50219224b98f0e2a5e31a9b82e9adbf
Beaconsyh08 <songyuhao2008@gmail.com>
2020-03-21 19:51:35 +0800

    update solution

--- Week3/Solution/WageCalculator.java
+++ Week3/Solution/WageCalculator.java
@@ -1,3 +1,18 @@
+/*
+   The University of Melbourne
+   School of Computing and Information Systems
+   COMP90041 Programming and Software Development
+*/
+
+/**
+ * Description
+ * This is not the official sample solution from the University
+ * If you have any confusion, please contact me
+ * yuhsong1@unimelb.edu.au
+ * yuhsong@student.unimelb.edu.au
+ *
+ * @author Yuhao Song
+ */
 public class WageCalculator {
     public static void main(String[] args) {
         int hours = Integer.parseInt(args[0]);

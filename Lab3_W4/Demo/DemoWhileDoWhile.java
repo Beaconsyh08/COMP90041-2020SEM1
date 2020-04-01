83bbbbcc5034b4b945f696570f707568ee4580ff
Beaconsyh08 <songyuhao2008@gmail.com>
2020-04-01 13:07:38 +0800

    lab3 update

---
+++ Lab3_W4/Demo/DemoWhileDoWhile.java
@@ -0,0 +1,18 @@
+/**
+ * Description
+ * This is for Demonstration purpose
+ * If you have any confusion, please contact me
+ * yuhsong1@unimelb.edu.au
+ * yuhsong@student.unimelb.edu.au
+ *
+ * @author Yuhao Song
+ */
+public class DemoWhileDoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i < 1);
+    }
+}

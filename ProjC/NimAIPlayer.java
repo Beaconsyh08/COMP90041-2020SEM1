9dacfc12d785dfd0761352c6db88583776a17986
Beaconsyh08 <songyuhao2008@gmail.com>
2020-05-18 17:50:09 +0800

    projC

---
+++ ProjC/NimAIPlayer.java
@@ -0,0 +1,24 @@
+/*
+	NimAIPlayer.java
+	
+	This class is provided as a skeleton code for the tasks of 
+	Sections 2.4, 2.5 and 2.6 in Project C. Add code (do NOT delete any) to it
+	to finish the tasks. 
+*/
+
+public class NimAIPlayer implements Testable {
+	// you may further extend a class or implement an interface
+	// to accomplish the tasks.	
+
+	public NimAIPlayer() {
+				
+	}
+	
+	public String advancedMove(boolean[] available, String lastMove) {
+		// the implementation of the victory
+		// guaranteed strategy designed by you
+		String move = "";
+		
+		return move;
+	}
+}
---
+++ ProjC/Testable.java
@@ -0,0 +1,10 @@
+/*
+	Testable.java
+	
+	This class is provided for auto-testing purpose. Do NOT change it. 
+	
+*/
+
+public interface Testable {
+	public String advancedMove(boolean[] available, String lastMove);
+}
